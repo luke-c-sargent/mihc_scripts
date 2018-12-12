b4df2ef3669a73eb735ce5cf73d5423015e7d9c6
Luke Sargent <luke.c.sargent@gmail.com>
2018-12-12 13:47:40 -0800

    okay packaging seems to work now -- on to history population

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .MIHCBase import MIHCBase
 from .MIHCGalaxy import MIHCGalaxy
 from .MIHCDataset import MIHCDataset
 from .MIHCGalaxyLibrary import MIHCGalaxyLibrary
+from .Detector import Detector
