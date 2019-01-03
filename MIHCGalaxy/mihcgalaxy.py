995281f7a14441f1d768a7ea20f0fc5243851720
Luke Sargent <luke.c.sargent@gmail.com>
2019-01-03 08:29:31 -0800

    adding workflow execution

--- MIHCGalaxy/mihcgalaxy.py
+++ MIHCGalaxy/mihcgalaxy.py
@@ -29,6 +29,7 @@ class MIHCGalaxy(MIHCBase):
     # samples = dict of MIHCData objects 
     # K: V == source directory: MIHCDataset Object
     for _s in samples: # each sample needs:
+      print(_s)
       _r = {}
       # a history
       _hname = samples[_s].get_data()["source_dir"].split('/')[-1]
@@ -36,8 +37,11 @@ class MIHCGalaxy(MIHCBase):
       # that history, populated
       _h.add_data(samples[_s], self._lib)
       _hinfo = _h.get_dataset_info()
-      _ds_info = _hinfo["datasets"]
-      _dsc_info = _hinfo["dataset_collections"]
+      _ds_info = _hinfo["datasets"] #list with hda_ldda, id, dataset_id
+      _dsc_info = _hinfo["dataset_collections"] #list with 
+      
+      #The map must be in the following format: {'<input_index>': {'id': <encoded dataset ID>, 'src': '[ldda, ld, hda, hdca]'}} (e.g. {'2': {'id': '29beef4fadeed09f', 'src': 'hda'}})
+      
       print("DS:\n{}\nDSC:\n{}\n!!!!!!!!!!!!!!!!!!!!!!!".format(_ds_info, _dsc_info))
       exit()
       # a workflow added
