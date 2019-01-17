de9b991ec873193540f4d7097a0361c40bb2a816
Luke Sargent <luke.c.sargent@gmail.com>
2019-01-17 13:47:39 -0800

    adding runner script, but this time in a different place

--- __init__.py
+++ __init__.py
@@ -5,5 +5,5 @@ from .MIHCGalaxyLibrary import MIHCGalaxyLibrary
 from .Detector import Detector
 from .MIHCRunner import MIHCRunner
 
-def run_mihc(location, key, address, port)
+def run_mihc(location, key, address, port):
   mihcrun = MIHCRunner(location, key, address, port)
